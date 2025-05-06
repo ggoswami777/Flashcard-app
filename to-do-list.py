a3704b76a12c5e9d6c2bd4615aea5da105a2aa34
Gaurav goswami <gaurav.goswami1304@gmail.com>
2025-05-06 18:10:58 +0530

    Add files via upload

---
+++ to-do-list.py
@@ -0,0 +1,24 @@
+def to_do_menu():
+    key=int(input("Press the key \n 1 -> To Add a task\n 2-> To View a Task \n 3-> Complete task \n 4-> Delete a task \n"))
+    if key==1:
+        addtask()
+    elif key==2:
+        view_task()
+    elif key==3:
+        complete_task()
+    elif key==4:
+        delete_task()
+    else:
+        print("Invalid Key!")
+def addtask():
+    pass
+def view_task():
+    pass
+def complete_task():
+    pass
+def delete_task():
+    pass
+
+to_do_menu()
+#the code is not fully functional yet!
+
